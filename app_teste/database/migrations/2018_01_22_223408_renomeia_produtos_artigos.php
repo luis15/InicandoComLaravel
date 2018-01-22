cc0181a4e9e36c160cc469fa360f46414091a593
luis15 <coelho.luisantonio@hotmail.com>
2018-01-22 20:39:17 -0200

    migrations

--- app_teste/database/migrations/2018_01_22_223408_renomeia_produtos_artigos.php
+++ app_teste/database/migrations/2018_01_22_223408_renomeia_produtos_artigos.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMigrationClient extends Migration
+class RenomeiaProdutosArtigos extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,9 @@ class CreateMigrationClient extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('produtos', function (Blueprint $table) {
+            Schema::rename('produtos', 'artigos');
+        });
     }
 
     /**
@@ -23,6 +25,8 @@ class CreateMigrationClient extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('produtos', function (Blueprint $table) {
+            Schema::rename('artigos','produtos');
+        });
     }
 }
